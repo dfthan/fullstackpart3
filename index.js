23fdb77ad5d846641057c1b62502b1a03548df44
dfthan <varahahmo1@gmail.com>
2022-04-14 17:59:18 +0300

    heroku 1.3

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const { response } = require('express')
 
 app.use(express.static('build'))
 app.use(express.json())
+
 app.use(cors())
 morgan.token("post", (req, res) => JSON.stringify(req.body))
 // Tiny ei enään käytössä, mutta oletan että tehtävässä 3.8 saa tehdä näin
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :post"))
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
@@ -19,7 +21,11 @@ app.get('/', (req, res) => {
 
 app.get("/info", (req, res) => {
     console.log(Date.now())
-    res.send(`Phonebook has info for ${persons.length} people <p> ${Date()} </p>`)
+    Person.estimatedDocumentCount()
+        .then(count => {
+            res.send(`Phonebook has info for ${count} people <p> ${Date()} </p>`
+            )
+        })
 })
 
 app.get('/api/persons', (req, res) => {
@@ -29,38 +35,31 @@ app.get('/api/persons', (req, res) => {
         })
 })
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
     Person.findById(req.params.id)
         .then(person => {
-            res.json(person)
-        })
-
-})
-
-app.delete("/api/persons/:id", (req, res, next) => {
-    Person.findById(req.params.id) 
-        .then(() => {
-            response.status(204).end()
+            if (person) {
+                res.json(person)
+            }
+            else {
+                res.status(404).end()
+            }
         })
         .catch(error => next(error))
 
 })
 
-app.post("/api/persons", (req, res) => {
-    //console.log("body", req.body)
+app.delete("/api/persons/:id", (req, res, next) => {
 
-    if (!req.body["name"] || !req.body["number"]) {
-        return res.status(400).json({
-            error: "Name or number missing!"
+    Person.findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.status(204).end()
         })
-    }
-    /*
-    else if (persons.find(person => person.name === req.body["name"])) {
-        return res.status(400).json({
-            error: "Name must be unique!"
-        })
-    }
-    */
+        .catch(error => next(error))
+
+})
+
+app.post("/api/persons", (req, res, next) => {
 
     const person = new Person({
         name: req.body["name"],
@@ -71,21 +70,49 @@ app.post("/api/persons", (req, res) => {
         .then(saved => {
             res.json(saved)
         })
+        .catch(error => next(error))
 
 })
 
-app.put("/api/persons/:id", (req,res,next) => {
+app.put("/api/persons/:id", (req, res, next) => {
     const person = {
         name: req.body["name"],
         number: req.body["number"],
     }
 
-    Person.findByIdAndUpdate(req.params.id,person, {new:true})
+    Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context:"query" })
         .then(updated => {
             res.json(updated)
         })
         .catch(error => next(error))
 })
+
+// Error handling 
+const badEndpoint = (req, res) => {
+    res.status(404).send({
+        error: "Wrong URL! try /api/persons"
+    })
+}
+
+app.use(badEndpoint)
+
+const handleError = (error, req, res, next) => {
+    console.log(error.message)
+
+    if (error.name === "CastError") {
+        return res.status(400).send({
+            error: `Bad id!`
+        })
+    }
+    if (error.name === "ValidationError") {
+        return res.status(400).send({
+            error: error.message
+        })
+    }
+    next(error)
+}
+
+app.use(handleError)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -11,9 +11,25 @@ mongoose.connect(url)
     })
 
     const personSchema = new mongoose.Schema({
-        name: String,
-        number: String,
+        name: {
+            type: String,
+            minlength: 3,
+            required: true,
+        },
+        number: {
+            type: String,
+            minlength: 8,
+            validate: {
+                validator: function(v) {
+                    // Numero minimissään joko 2+6 tai 3+5 pituudelta eli 8
+                    return /\d{2}-\d{6}/.test(v) || /\d{3}-\d{5}/.test(v)
+                },
+                message: "Not a valid number! Please enter in xx-xxxx or xxx-xxxx format"
+            },
+            required: true
+        }
     })
+
 
     personSchema.set("toJSON", {
         transform: (document, returned) => {
